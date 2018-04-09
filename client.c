caed53c9262fa12d437a0a6bd4cd61d92da6201f
sunshangjih <sunshangjih@sunshangjihde-MacBook-Pro.local>
2018-04-09 14:37:31 +0800

    debuging

---
+++ client.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[]){
+	//socket setup
+	int sockfd = 0;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if(sockfd == -1){
+		printf("[Client] socket setup Error!\n");
+	}
+
+	//socket connection
+	struct sockaddr_in info;
+	bzero(&info, sizeof(info));
+	info.sin_family = PF_INET;
+
+	info.sin_addr.s_addr = inet_addr(argv[3]);
+	info.sin_port = htons(argv[4]);
+	printf("[Client] IP: %s\n",argv[3]);
+	printf("[Client] Port: %s\n",argv[4]);
+	
+	int err = connect(sockfd, (struct sockaddr *)&info, sizeof(info));
+	if(err == -1){
+		printf("[Client] Connection error!\n");
+	}
+	else{
+		printf("[Client] Server connected.\n");
+		//File transfer
+		FILE *fp = fopen(argv[5], "wb");
+		char buffer[256] = {};
+		int nCount;
+		if(!strcmp("recv",argv[2]))
+		{
+			printf("[Client] Recv Start.\n");
+			while((nCount = recv(sockfd, buffer, sizeof(buffer), 0)) > 0){
+				fwrite(buffer, sizeof(buffer), nCount, fp);
+			}
+		}
+		fclose(fp);
+	}
+	close(sockfd);
+	return 0;
+}
--- server.c
+++ server.c
@@ -9,61 +9,68 @@
 int main(int argc , char *argv[])
 
 {
-    if(!strcmp(argv[0],"tcp")){
-        if(!strcmp(argv[1],"send")){
-            //socket set up
-            char buffer[256] = {};
-            int sockfd = 0,forClientSockfd = 0;
-            sockfd = socket(AF_INET , SOCK_STREAM , 0);
+	if(!strcmp(argv[1],"tcp")){
+		printf("[Server] tcp\n");
+		//socket set up
+		char buffer[256] = {};
+		int sockfd = 0, forClientSockfd = 0;
+		sockfd = socket(AF_INET , SOCK_STREAM , 0);
 
-            if (sockfd == -1){
-                printf("ERROR: Fail to create a socket.\n");
-            }
+		if (sockfd == -1){
+			printf("ERROR: Fail to create a socket.\n");
+		}
 
-            //socket connect
-            struct sockaddr_in serverInfo,clientInfo;
-            int addrlen = sizeof(clientInfo);
-            bzero(&serverInfo,sizeof(serverInfo));
+		//socket connect
+		struct sockaddr_in serverInfo, clientInfo;
+		int addrlen = sizeof(clientInfo);
+		bzero(&serverInfo,sizeof(serverInfo));
 
-            serverInfo.sin_family = PF_INET;
-            serverInfo.sin_addr.s_addr = inet_addr(argv[2]);
-            serverInfo.sin_port = htons(argv[3]);
-            bind(sockfd,(struct sockaddr *)&serverInfo,sizeof(serverInfo));
-            listen(sockfd,5);
+		serverInfo.sin_family = PF_INET;
+		serverInfo.sin_addr.s_addr = inet_addr(argv[3]);
+		serverInfo.sin_port = htons(argv[4]);
+		bind(sockfd,(struct sockaddr *)&serverInfo,sizeof(serverInfo));
+		listen(sockfd,5);
 
-            //Accept client
-            forClientSockfd = accept(sockfd,(struct sockaddr*) &clientInfo, &addrlen);
-            if(forClientSockfd == -1){
-                printf("ERROR: Fail to accept a request.\n");
-            }
-            //Sending file
-            else{
-                //File open
-                char *filename = argv[4];
-                File *fp = fopen(filename, "rb");
-                if(fp == NULL){
-                    printf("ERROR: File opening error.\n");
-                }
+		//Accept client
+		printf("[Server] Waiting for Connection...\n");
+		forClientSockfd = accept(sockfd,(struct sockaddr*) &clientInfo, &addrlen);
+		
+		printf("[Server] Server connected.\n");
+		
+		if(forClientSockfd == -1){
+			printf("ERROR: Fail to accept a request.\n");
+		}
+		
+		//Sending file
+		else{
+			//File open
+			char *filename = argv[5];
+			FILE *fp = fopen(filename, "rb");
+			if(fp == NULL){
+				printf("ERROR: File opening error.\n");
+			}
 
-                //Send process
-                int nCount;
-                while((nConut = fread(buffer, 1, sizeof(buffer))) > 0){
-                    send(clientInfo, buffer, nCount, 0);
-                }
-                printf("[Server] Done.\n");
-                shutdown(clientInfo, SD_SEND);
-                read(clientInfo, buffer, nCount, 0);
+			//Send process
+			//1byte
+			int nCount;
+			if(!strcmp(argv[2],"send")){
+				printf("[Server] Send\n");
+				while((nCount = fread(buffer, 1, sizeof(buffer), fp)) > 0){
+					send(forClientSockfd, buffer, nCount, 0);
+				}
+				printf("[Server] Done.\n");
+				recv(forClientSockfd, buffer, nCount, 0);
 
-                //closing 
-                fclose(fp);
-                close(serverInfo);
-                close(clientInfo);
-            }
-        }
-        else if(!strcmp(argv[1],"recv")){
-        }
-    }
-    else if(!strcmp(argv[0],"udp")){
-    }
-    return 0;
+				//closing 
+				fclose(fp);
+				close(sockfd);
+				close(forClientSockfd);
+			}
+			else if(!strcmp(argv[2],"recv")){
+			}
+		}
+	}
+	else if(!strcmp(argv[1],"udp")){
+	}
+	return 0;
 }
